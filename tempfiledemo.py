5dce4a2329495aeb8d4947a5ed58259ab0d2ad68
Helge Stenström <helge.stenstrom@ericsson.com>
2017-08-31 16:01:04 +0200

    Window cleaner
    Genom att stänga filen i samma funktion som öppnar den, och inte i tearDown, så undviker jag ResourceWarning.
    
    Just nu har jag (lite fult) flera close() på filen. Det går tydligen bra utan att få varningar. Är det för att det inte är en vanlig fil?

--- tempfiledemo.py
+++ tempfiledemo.py
@@ -3,41 +3,47 @@
 import tempfile
 import unittest
 
+
 class ClassToBeTested:
     def __init__(self, f):
         self.file = f
         
-    def functionToBeTested(self):
+    def function_to_be_tested(self):
         fd = open(self.file, "rb")
-        
-        return fd.read()
+        print("open and read: ", self.file)
+        contents = fd.read()
+        fd.close()
+        return contents
+
 
 # Unit testing
-
 class SomeTests(unittest.TestCase):
     def setUp(self):
         self.thefile = tempfile.NamedTemporaryFile()
+        print("create: ", self.thefile.name)
         self.c = ClassToBeTested(self.thefile.name)
 
     def tearDown(self):
+        print("closing ", self.thefile.name)
         self.thefile.close()
-
-    # def test_that_fails(self):
-    #     self.fail()
 
     def test_that_file_is_read(self):
         # setup
         self.thefile.write(b"abc")
         self.thefile.flush()
-        
-        result = self.c.functionToBeTested()
-        self.assertEqual(result, b"abc")
-        pass
 
-    
+        # exercise
+        result = self.c.function_to_be_tested()
+        # On windows, this gives a ResourceWarning, because the file
+        # is opened for read, but not closed. The file is closed
+        # later, by tearDown.
+
+        print("self.thefile: ", self.thefile)
+        self.thefile.close()
+
+        # verify
+        self.assertEqual(result, b"abc")
 
 
 if __name__ == "__main__":
     unittest.main()
-
-    
