78cd37b79bc5c2d75338a50406c1c7fe4e5f8553
Helge Stenström <h.stenstrom@gmail.com>
2017-09-22 23:11:50 +0200

    Städning

--- finddups_tests.py
+++ finddups_tests.py
@@ -3,6 +3,18 @@ import unittest
 import finddups
 
 
+def filterFileList(aList):
+    filteredList = [n for n in aList if "DS" not in n]
+    return filteredList
+
+
+class UtilityTests(unittest.TestCase):
+
+    def test_that_DS_are_removed(self):
+        l = ['a', 'xDSx', 'b']
+        self.assertEqual(['a', 'b'], filterFileList(l))
+
+
 class FunctionTests(unittest.TestCase):
     """tests various aspects of the Example class"""
 
@@ -10,12 +22,19 @@ class FunctionTests(unittest.TestCase):
         """A test that should be successful"""
         pass
 
-    def testF(self):
-        startDir = "."
+    def test_that_files_are_found(self):
+        # Setup
+        startDir = "exampleFiles"
 
-        expected = ["x"]
-        actual = finddups.getFileList(startDir)
-        self.assertEqual(expected, actual)
+        # Exercise
+        aList = finddups.getFileList(startDir)
+
+        # Post process for beter determinism
+        filteredList = filterFileList(aList)
+
+        # Verify
+        self.assertIn("exampleFiles/adir/a.txt", filteredList, "find a file that is in the folder")
+        self.assertEqual(4, len(filteredList), "Number of files in example folder")
 
 
 if __name__ == '__main__':
